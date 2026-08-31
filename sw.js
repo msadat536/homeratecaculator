77fafb9cbb7b6c834768861cf10be0cab185c706
msadat536 <45773994+msadat536@users.noreply.github.com>
2026-08-31 00:38:08 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'homecheck-v19';
+const CACHE = 'homecheck-v20';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {

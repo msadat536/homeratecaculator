83b98a4905f0dc9b00129329932fbaa6b8055ae7
msadat536 <45773994+msadat536@users.noreply.github.com>
2026-09-08 10:00:09 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ self.addEventListener('fetch', e => {
     return;
   }
 });
-const CACHE = 'homecheck-v98';
+const CACHE = 'homecheck-v99';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
